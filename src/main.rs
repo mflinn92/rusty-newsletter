cd6327d5698c21335f0883d3a61a421bc98190b5
mflinn92 <mflinn92@gmail.com>
2021-06-02 08:38:18 -0400

    tests for status check

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,7 @@
-use actix_web::{web, App, HttpRequest, HttpResponse, HttpServer, Responder};
-
-async fn health_check(_: HttpRequest) -> impl Responder {
-    HttpResponse::Ok().finish()
-}
+use newsletter::run;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().route("/status", web::get().to(health_check)))
-        .bind("127.0.0.1:8000")?
-        .run()
-        .await
+    let listener = std::net::TcpListener::bind("127.0.0.1:8000")?;
+    run(listener)?.await
 }
